2ba090999b31123da30e0706e4c45ee203dd931e
Gefelix <128931864+devindonadoni@users.noreply.github.com>
2025-02-20 23:24:48 +0100

    index animation

---
+++ js/universal-script.js
@@ -0,0 +1,22 @@
+function toggleMobileMenu() {
+    var menu = document.getElementById("mobile-menu");
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden");
+        menu.classList.add("visible");
+    } else {
+        menu.classList.remove("visible");
+        menu.classList.add("hidden");
+    }
+}
+
+
+
+// Avvia la barra di caricamento appena viene avviato il caricamento della pagina
+NProgress.start();
+
+NProgress.configure({ showSpinner: false });
+
+// Al termine del caricamento della pagina, termina la barra
+window.addEventListener('load', function () {
+    NProgress.done();
+});
